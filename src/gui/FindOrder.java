eaeab726f4adc383c36fe02c7beecfdf22387ef1
tony-kong <tonykongg@gmail.com>
2018-06-16 16:24:03 -0700

    updated find order

--- src/gui/FindOrder.java
+++ src/gui/FindOrder.java
@@ -14,19 +14,29 @@ import javax.swing.JTable;
 import app.OrderSql;
 import model.OrderDelivers;
 
+@SuppressWarnings("serial")
 public class FindOrder extends JFrame implements ActionListener {
+
+	private static final String[] COLUMN_NAMES = { "Order#", "Delivery Method", "Start Date", "Days to deliver" };
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String userEnter = JOptionPane.showInputDialog(null, "Enter The Order #");
-		int orderId = Integer.parseInt(userEnter);
+		int orderId;
+
+		try {
+			orderId = Integer.parseInt(userEnter);
+		} catch (NumberFormatException ex) {
+			JOptionPane.showMessageDialog(null, "Please enter a valid integer", "Error",
+					JOptionPane.INFORMATION_MESSAGE);
+			return;
+		}
+
 		List<OrderDelivers> orders = OrderSql.selectOrderFromId(orderId);
 
 		setLayout(new FlowLayout());
 
 		String[][] data = new String[orders.size()][4];
-
-		String[] columnNames = { "Order#", "Delivery Method", "Start Date", "Days to deliver" };
 
 		if (orders.size() != 0) {
 			for (int i = 0; i < orders.size(); i++) {
@@ -35,7 +45,7 @@ public class FindOrder extends JFrame implements ActionListener {
 				data[i][2] = "" + orders.get(i).getStartDate();
 				data[i][3] = "" + orders.get(i).getDeliveryDays();
 			}
-			JTable table = new JTable(data, columnNames);
+			JTable table = new JTable(data, COLUMN_NAMES);
 			table.setPreferredScrollableViewportSize(new Dimension(1024, 768));
 			table.setFillsViewportHeight(true);
 
