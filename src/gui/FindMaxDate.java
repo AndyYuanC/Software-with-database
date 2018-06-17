a16e821711f37ce0daf6bd52fdaf2488230370ef
Matthew <Matthew@dhcp-206-87-214-162.ubcsecure.wireless.ubc.ca>
2018-06-16 17:50:42 -0700

    FindMinDate edited, and added FindMaxDate

--- src/gui/FindMaxDate.java
+++ src/gui/FindMaxDate.java
@@ -7,26 +7,25 @@ import javax.swing.JOptionPane;
 
 import app.OrderSql;
 
-public class FindMinDate implements ActionListener {
+public class FindMaxDate implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		int minDate = 0;
+		int maxDate = 0;
 		String DeliveryMethod = "";
 		boolean valid = false;
 		
 		while(!valid) {
 				Object[] possibilities = {"Standard", "Express", "Priority"};
-				DeliveryMethod = (String)JOptionPane.showInputDialog(null, "Choose one of the Delivery Method", "Find Minimum Date",JOptionPane.PLAIN_MESSAGE,null, possibilities,"Standard");
+				DeliveryMethod = (String)JOptionPane.showInputDialog(null, "Choose one of the Delivery Method", "Find Maximum Date",JOptionPane.PLAIN_MESSAGE,null, possibilities,"Standard");
 				if(DeliveryMethod != null&&(DeliveryMethod.equals("Express")||DeliveryMethod.equals("Standard")||DeliveryMethod.equals("Priority"))) {
 					valid = true;
-					JOptionPane.showMessageDialog(null, "The Min Date of the " + DeliveryMethod + " delivery method is: " + OrderSql.minDaysDeliveryMethod(DeliveryMethod) + " day(s)", "Error", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(null, "The Maximum Date of the " + DeliveryMethod + " delivery method is: " + OrderSql.maxDaysDeliveryMethod(DeliveryMethod) + " day(s)", "Error", JOptionPane.INFORMATION_MESSAGE);
 			}else {
 				JOptionPane.showMessageDialog(null, "Enter The Valid Delivery Method (Express, Regular or Priority):", "Error",
 						JOptionPane.INFORMATION_MESSAGE);
 			}
 		}
 	}
-
 }
