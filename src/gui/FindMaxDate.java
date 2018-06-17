fe773712e4cd7d4f978bf745cc118f27ff8554ac
tony-kong <tonykongg@gmail.com>
2018-06-16 17:54:06 -0700

    cleanup

--- src/gui/FindMaxDate.java
+++ src/gui/FindMaxDate.java
@@ -11,20 +11,23 @@ public class FindMaxDate implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		int maxDate = 0;
 		String DeliveryMethod = "";
 		boolean valid = false;
-		
-		while(!valid) {
-				Object[] possibilities = {"Standard", "Express", "Priority"};
-				DeliveryMethod = (String)JOptionPane.showInputDialog(null, "Choose one of the Delivery Method", "Find Maximum Date",JOptionPane.PLAIN_MESSAGE,null, possibilities,"Standard");
-				if(DeliveryMethod != null&&(DeliveryMethod.equals("Express")||DeliveryMethod.equals("Standard")||DeliveryMethod.equals("Priority"))) {
-					valid = true;
-					JOptionPane.showMessageDialog(null, "The Maximum Date of the " + DeliveryMethod + " delivery method is: " + OrderSql.maxDaysDeliveryMethod(DeliveryMethod) + " day(s)", "Error", JOptionPane.INFORMATION_MESSAGE);
-			}else {
-				JOptionPane.showMessageDialog(null, "Enter The Valid Delivery Method (Express, Regular or Priority):", "Error",
-						JOptionPane.INFORMATION_MESSAGE);
+
+		while (!valid) {
+			Object[] possibilities = { "Standard", "Express", "Priority" };
+			DeliveryMethod = (String) JOptionPane.showInputDialog(null, "Choose one of the Delivery Method",
+					"Find Maximum Date", JOptionPane.PLAIN_MESSAGE, null, possibilities, "Standard");
+			if (DeliveryMethod != null && (DeliveryMethod.equals("Express") || DeliveryMethod.equals("Standard")
+					|| DeliveryMethod.equals("Priority"))) {
+				valid = true;
+				JOptionPane.showMessageDialog(null,
+						"The Maximum Date of the " + DeliveryMethod + " delivery method is: "
+								+ OrderSql.maxDaysDeliveryMethod(DeliveryMethod) + " day(s)",
+						"Maximum Date", JOptionPane.INFORMATION_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null, "Enter The Valid Delivery Method (Express, Regular or Priority):",
+						"Error", JOptionPane.INFORMATION_MESSAGE);
 			}
 		}
 	}
--- src/gui/FindMinDate.java
+++ src/gui/FindMinDate.java
@@ -11,20 +11,23 @@ public class FindMinDate implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		int minDate = 0;
 		String DeliveryMethod = "";
 		boolean valid = false;
-		
-		while(!valid) {
-				Object[] possibilities = {"Standard", "Express", "Priority"};
-				DeliveryMethod = (String)JOptionPane.showInputDialog(null, "Choose one of the Delivery Method", "Find Minimum Date",JOptionPane.PLAIN_MESSAGE,null, possibilities,"Standard");
-				if(DeliveryMethod != null&&(DeliveryMethod.equals("Express")||DeliveryMethod.equals("Standard")||DeliveryMethod.equals("Priority"))) {
-					valid = true;
-					JOptionPane.showMessageDialog(null, "The Min Date of the " + DeliveryMethod + " delivery method is: " + OrderSql.minDaysDeliveryMethod(DeliveryMethod) + " day(s)", "Error", JOptionPane.INFORMATION_MESSAGE);
-			}else {
-				JOptionPane.showMessageDialog(null, "Enter The Valid Delivery Method (Express, Regular or Priority):", "Error",
-						JOptionPane.INFORMATION_MESSAGE);
+
+		while (!valid) {
+			Object[] possibilities = { "Standard", "Express", "Priority" };
+			DeliveryMethod = (String) JOptionPane.showInputDialog(null, "Choose one of the Delivery Method",
+					"Find Minimum Date", JOptionPane.PLAIN_MESSAGE, null, possibilities, "Standard");
+			if (DeliveryMethod != null && (DeliveryMethod.equals("Express") || DeliveryMethod.equals("Standard")
+					|| DeliveryMethod.equals("Priority"))) {
+				valid = true;
+				JOptionPane.showMessageDialog(null,
+						"The Min Date of the " + DeliveryMethod + " delivery method is: "
+								+ OrderSql.minDaysDeliveryMethod(DeliveryMethod) + " day(s)",
+						"Minimal Date", JOptionPane.INFORMATION_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null, "Enter The Valid Delivery Method (Express, Regular or Priority):",
+						"Error", JOptionPane.INFORMATION_MESSAGE);
 			}
 		}
 	}
